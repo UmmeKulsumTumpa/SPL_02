e17e712484c015a1ff5a83ffeceaf0f70e835a60
Mosamma Sultana Trina <114928172+Trina-SE@users.noreply.github.com>
2024-06-09 05:23:25 +0600

    Update index.js

--- CustomProblem/backend/index.js
+++ CustomProblem/backend/index.js
@@ -1,27 +1,22 @@
 const express = require('express');
-const cors = require('cors'); // Import CORS middleware
+const cors = require('cors');
 const mongoose = require('mongoose');
 const addProblemRouter = require('./routes/addProblem');
+const submitSolutionRouter = require('./routes/submitSolution');
 
 const app = express();
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://bsse1307:bsse1307@cluster0.nqcakei.mongodb.net/codesphere?retryWrites=true&w=majority&appName=Cluster0', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Error connecting to MongoDB:', err));
+mongoose.connect('mongodb+srv://bsse1307:bsse1307@cluster0.nqcakei.mongodb.net/codesphere?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
 
-// Middleware
-app.use(cors()); // Use CORS middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
 app.use('/addProblem', addProblemRouter);
+app.use('/submitSolution', submitSolutionRouter);
 
-// Start the server
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
