0184681459d9a5eb594ce68afd934ff266589263
Mosamma Sultana Trina <114928172+Trina-SE@users.noreply.github.com>
2024-06-09 04:43:22 +0600

    Update code.cpp

--- Custom Problem/executor/code.cpp
+++ Custom Problem/executor/code.cpp
@@ -2,8 +2,15 @@
 using namespace std;
 
 int main() {
+
+    int t;
+    cin>>t;
+    while(t--){
+        
     int a, b;
     cin >> a >> b;
     cout << a + b << endl;
+        
+    }
     return 0;
 }
