40a566a9c6b96edb73fd29dbf9a34e44aae9167c
Mosamma Sultana Trina <114928172+Trina-SE@users.noreply.github.com>
2024-06-10 04:13:56 +0600

    CS/6T (solution backend check_upload sumtest.cpp file)

---
+++ CustomProblem/testCases/sumtest.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin>>t;
+   // t = t/0;
+  // while(1){};
+    while(t--){
+        int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    }
+    return 0;
+}
