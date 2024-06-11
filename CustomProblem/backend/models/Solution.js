e6b46ab5ef1f875f2f657d2a6ad95156eb377df7
Mosamma Sultana Trina <114928172+Trina-SE@users.noreply.github.com>
2024-06-11 15:47:14 +0600

    Memory Added

--- CustomProblem/backend/models/Solution.js
+++ CustomProblem/backend/models/Solution.js
@@ -3,10 +3,12 @@ const { v4: uuidv4 } = require('uuid');
 
 const solutionSchema = new mongoose.Schema({
   sid: { type: String, default: uuidv4, unique: true },
-  problem: { type: mongoose.Schema.Types.ObjectId, ref: 'TempProblem', required: true },
+  problem: { type: mongoose.Schema.Types.ObjectId, ref: 'CustomProblem', required: true },
   solutionCode: { type: String, required: true },
   verdict: { type: String, default: 'Pending' },
   execTime: { type: Number, default: 0 },
+  maxMemoryUsageMB: { type: Number, default: 0 },
+  output: { type: [String], default: [] }
 });
 
 const Solution = mongoose.model('Solution', solutionSchema);
